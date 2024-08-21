cef7b33b10f9c04c8bdc459abec170b407e2cb29
Abu Obayed <obayed15-6250@s.diu.edu.bd>
2024-08-21 22:42:44 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,99 @@
-// Current SDK version: 3.22.1
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_project/home.dart';
-import 'package:flutter_project/profile_page.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/rendering.dart';
 
-// TextField
-
-void main() {
-  runApp(IntroApp());
+void main(){
+  runApp( DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
-
-class IntroApp extends StatelessWidget {
-  const IntroApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      debugShowCheckedModeBanner: true,
-      home: ProfilePage(),
-      title: 'Intro app',
-      // theme: ThemeData(
-      //     brightness: Brightness.light,
-      //     appBarTheme: AppBarTheme(
-      //         backgroundColor: Colors.pink,
-      //         titleTextStyle: TextStyle(
-      //             color: Colors.white,
-      //             fontSize: 22,
-      //             fontWeight: FontWeight.w700,
-      //             letterSpacing: 1.2)),
-      //     textButtonTheme: TextButtonThemeData(
-      //         style: TextButton.styleFrom(
-      //             foregroundColor: Colors.pink,
-      //             textStyle:
-      //                 TextStyle(fontSize: 16, fontWeight: FontWeight.w700))),
-      //     inputDecorationTheme: InputDecorationTheme(
-      //       border: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.green, width: 2)),
-      //       disabledBorder: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.green, width: 2)),
-      //       focusedBorder: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.blueAccent, width: 2)),
-      //       labelStyle: TextStyle(fontSize: 16),
-      //       hintStyle: TextStyle(color: Colors.purple.shade200),
-      //     ),
-      //     textTheme: TextTheme(bodySmall: TextStyle(fontSize: 28))),
-      // darkTheme: ThemeData(
-      //     brightness: Brightness.dark,
-      //     appBarTheme: AppBarTheme(
-      //         backgroundColor: Colors.green,
-      //         titleTextStyle: TextStyle(
-      //             color: Colors.white,
-      //             fontSize: 22,
-      //             fontWeight: FontWeight.w700,
-      //             letterSpacing: 1.2)),
-      //     textButtonTheme: TextButtonThemeData(
-      //         style: TextButton.styleFrom(
-      //             foregroundColor: Colors.green,
-      //             textStyle:
-      //                 TextStyle(fontSize: 16, fontWeight: FontWeight.w700))),
-      //     inputDecorationTheme: InputDecorationTheme(
-      //       border: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.green, width: 2)),
-      //       disabledBorder: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.green, width: 2)),
-      //       focusedBorder: OutlineInputBorder(
-      //           borderRadius: BorderRadius.circular(8),
-      //           borderSide: BorderSide(color: Colors.blueAccent, width: 2)),
-      //       labelStyle: TextStyle(fontSize: 16),
-      //       hintStyle: TextStyle(color: Colors.purple.shade200),
-      //     ),
-      //     textTheme: TextTheme(
-      //         bodySmall: TextStyle(fontSize: 20),
-      //         titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-      //         titleMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-      //         titleSmall:
-      //             TextStyle(fontSize: 18, fontWeight: FontWeight.w600))),
-      // themeMode: ThemeMode.dark,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: Home(),
+    );
+  }
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //MediaQuery//
+
+    MediaQueryData mediaQuery =MediaQuery.of(context);
+    print(mediaQuery.size.height);
+    print(mediaQuery.size.width);
+    print(mediaQuery.size.aspectRatio);
+    print(mediaQuery.size.flipped.height);
+    print(mediaQuery.devicePixelRatio);
+
+    if(mediaQuery.size.width<640){
+      print("this is phone");
+    }else if(mediaQuery.size.width>640&&mediaQuery.size.width<1008){
+      print("this is tablet");
+    }else{
+      print('this is laptop');
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.orange,
+      ),
+      // body: OrientationBuilder(
+      //   builder: (context,Orientation orientation) {
+      //     print(orientation);
+      //     if (orientation == Orientation.portrait) {
+      //       return Column(
+      //         children: [
+      //           Wrap(
+      //             alignment: WrapAlignment.center,
+      //             spacing: 14,
+      //             children: [
+      //               Text('hds dshllv dshdh sfnsb dhds'),
+      //               Text('hds dshllv dshdhsfnsbdhds'),
+      //               Text('hds dshllv dshdhsf nsbdhds'),
+      //               Text('hds  dshllv dshdhsfn sbdhds'),
+      //               Text('hds  dshllv dshd hsfnsbd hds'),
+      //               Text('hds  dshllv dshd hsfns bdhds'),
+      //               Text('hds  dshllv dsh dhsf nsb dhds'),
+      //               Text('hds  dshllv dshdh sfnsb dhds'),
+      //               Text('hds  dshllv ds hdh sfnsb  dhds'),
+      //               Text('hds  dshllv dsh dhs      fns bdhds'),
+      //               Text('hds  dshllv bdhds'),
+      //             ],
+      //           )
+      //         ],
+      //       );
+      //     }else{
+      //       return Center(
+      //         child: Text("Too big vai"),
+      //       );
+      //     }
+      //   }
+      // ),
+      body: LayoutBuilder(
+        builder: (context,BoxConstraints constraints){
+          if(constraints.maxWidth<640){
+            return Text("this is phone");
+          }else if(constraints.maxWidth>640&&constraints.maxWidth<1008){
+            return Text("this is tablet");
+          }else{
+            return Text('this is laptop');
+          }
+      }
+      ),
     );
   }
 }
